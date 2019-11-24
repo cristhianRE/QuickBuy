535d66e3682ca6a4e2d6e5cf1dd5e5f560fedee4
Cristhian Ebert <cristhianREbert@gmail.com>
2019-11-24 09:51:04 -0300

    Contexto de persistência e classes de configuração de mapeamento

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -10,11 +10,13 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
         public DateTime DataPrevisaoEntrega { get; set; }
         public string CEP { get; set; }
         public string Estado { get; set; }
         public string Cidade { get; set; }
         public string EnderecoCompleto { get; set; }
+        public string NumeroEndereco { get; set; }
         public int FormaPagamentoId { get; set; }
         public FormaPagamento FormaPagamento { get; set; }
         public ICollection<Usuario> ItensPedido { get; set; }
